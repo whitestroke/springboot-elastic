e06e690d008e224dc59986d9fb6fd98a492f7688
lct <2544580976@qq.com>
2024-04-09 11:23:41 +0800

    1. 代码修改

--- src/main/java/com/lct/springbootjar/springbootes/service/elastic/ElasticService.java
+++ src/main/java/com/lct/springbootjar/springbootes/service/elastic/ElasticService.java
@@ -1,13 +1,16 @@
 package com.lct.springbootjar.springbootes.service.elastic;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.util.ObjectBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.function.Function;
 
 /**
  * @author :lct
@@ -24,15 +27,28 @@ public class ElasticService {
     }
 
     public void getInfoForIndex(String indexName, Integer page, Integer size) throws IOException {
-        SearchResponse<String> search = esClient.search(s -> s
+        /*SearchResponse<String> search = esClient.search(s -> s
                         .index(indexName)
                         .from((page - 1) * size)
                         .size(size)
                         .query(q -> q
                                 .matchAll(t -> t.boost(1F))),
-                String.class);
+                String.class);*/
 
-        for (Hit<String> hit: search.hits().hits()) {
+
+
+        SearchRequest searchRequest = SearchRequest.of(s -> s
+                .index(indexName)
+                .from((page - 1) * size)
+                .size(size)
+                .query(q -> q
+                        .matchAll(t -> t.boost(1F))));
+        String string = searchRequest.toString();
+        System.out.println(string);
+
+        SearchResponse<String> search1 = esClient.search(searchRequest, String.class);
+
+        for (Hit<String> hit: search1.hits().hits()) {
             System.out.println(hit.source());
         }
     }
